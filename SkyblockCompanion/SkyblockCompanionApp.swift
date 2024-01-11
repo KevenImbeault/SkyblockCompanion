725990eb14fbbf8b491e446d3e77554f50f79d96
KevenImbeault <imbeault@gmail.com>
2024-01-11 11:12:31 -0500

    Initial Commit

---
+++ SkyblockCompanion/SkyblockCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyblockCompanionApp.swift
+//  SkyblockCompanion
+//
+//  Created by kimbeault on 2024-01-11.
+//
+
+import SwiftUI
+
+@main
+struct SkyblockCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
